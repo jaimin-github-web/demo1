2e84808e6f82202fa71e494856d5383845c199dd
No Name <jaimin@rivuletdigital.com>
2024-02-19 08:01:43 +0000

    Updated 1 file via CloudCannon.

--- cloudcannon.config.js
+++ cloudcannon.config.js
@@ -1,10 +1,26 @@
 module.exports = {
   collections_config: {
     posts: {
-      path: "content/posts",
+      path: "src/_posts",
       output: true,
       url: "/posts/{category|slugify}/[slug].html",
       icon: "event_available"
+    },
+    pages: {
+      // Tells CloudCannon to navigate to this path for this collection
+      path: 'pages',
+      output: true,
+      url: "/[...blog]/{category|slugify}/[slug].astro"
+
+      // Reads the contents of each file for this pattern (takes priority over path)
+      glob: ['**/*.md', './src/pages/*.astro'],
+
+      // Tells CloudCannon to only show successfully parsed files for this collection
+      // Useful for excluding other collections when using '' as path
+      filter: 'strict',
+
+      // Tells CloudCannon this collection produces output files
+      output: true
     }
   }
 };
