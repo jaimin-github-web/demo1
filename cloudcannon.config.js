250560855c91c4a23e85fc7d45c4118dd73679dd
No Name <jaimin@rivuletdigital.com>
2024-02-19 11:36:41 +0000

    Updated 1 file via CloudCannon.

--- cloudcannon.config.js
+++ cloudcannon.config.js
@@ -1,24 +1,21 @@
-// module.exports = {
-//   collections_config: {
-//     posts: {
-//       path: "src/_posts",
-//       output: true,
-//       url: "/posts/{category|slugify}/[slug]",
-//       icon: "event_available"
-//     },
-//     pages: {
-//       name: 'Pages',
-//       path: 'src/pages',
-//       glob: ['**/*.md', './src/pages/*.astro'],
-//       parser: 'frontmatter',
-//       output: true,
-//       url: '/[slug]'
-//     },
-//   }
-// };
-
 module.exports = {
-  _editables: {
+  collections_config: {
+    posts: {
+      path: "src/_posts",
+      output: true,
+      url: "/posts/{category|slugify}/[slug]",
+      icon: "event_available"
+    },
+    pages: {
+      name: 'Pages',
+      path: 'src/pages',
+      glob: ['**/*.md', './src/pages/*.astro'],
+      parser: 'frontmatter',
+      output: true,
+      url: '/[slug]'
+    },
+  },
+   _editables: {
     text: {
       italic: true
     },
