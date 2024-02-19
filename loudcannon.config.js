f166daa3d0ba56cbd22cb0106a3bce78e4906331
No Name <jaimin@rivuletdigital.com>
2024-02-19 07:28:30 +0000

    Added 1 file via CloudCannon.

---
+++ loudcannon.config.js
@@ -0,0 +1,16 @@
+paths:
+  uploads: uploads
+  static: ''
+  collections: ''
+  data: ''
+  layouts:
+collections_config: {}
+collection_groups:
+  - heading:
+    collections:
+_inputs: {}
+_select_data: {}
+_structures: {}
+_editables: {}
+data_config: {}
+timezone: Etc/UTC
